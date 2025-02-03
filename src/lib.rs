dc7aed58f60599309c80f3d82bd5eb167d45e08c
theteachr <sop.theteachr@gmail.com>
2025-02-03 16:30:59 +0530

    Refactor database schema and error handling
    - Renames `uri` to `url`
    - Removes some static strings in favor of `Box<dyn Error>` values
    - Dries up sqlite client creation
    - Updates postgres to use `hackathon_raptors` database
    - Reuses schema

--- src/lib.rs
+++ src/lib.rs
@@ -10,14 +10,14 @@ use url::Url;
 pub trait Shrinker {
     fn shrink(&mut self, url: Url) -> Result<String, error::Internal>;
     fn expand(&self, code: &str) -> Result<Url, error::Load>;
-    fn store_custom(&mut self, uri: Url, code: &str) -> Result<(), error::Storage>;
+    fn store_custom(&mut self, url: Url, code: &str) -> Result<(), error::Storage>;
 }
 
 trait Generator {
-    fn generate(&mut self, uri: &Url) -> String;
+    fn generate(&mut self, url: &Url) -> String;
 }
 
 trait Storage {
-    fn store(&mut self, uri: Url, code: &str) -> Result<(), error::Storage>;
+    fn store(&mut self, url: Url, code: &str) -> Result<(), error::Storage>;
     fn load(&self, code: &str) -> Result<Url, error::Load>;
 }
---
+++ src/storage/db/scripts/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS urls (
+  code TEXT PRIMARY KEY,
+  url TEXT NOT NULL
+);
